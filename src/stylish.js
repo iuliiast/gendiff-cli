49f76b4c8c83948dddd6ca48d95c9de4f96da213
Yulia Beton <yuliabeton@gmail.com>
2020-07-25 18:15:03 +0300

    fix render

--- src/stylish.js
+++ src/stylish.js
@@ -1,31 +1,41 @@
 const formatter = (file) => {
   let result = '';
+
+  const checkValue = (obj) => {
+    if (typeof obj.value === 'object') {
+      const entries = (Object.entries(obj.value)).flat();
+      const createFormWithObj = `{\n ${entries[0]} : ${entries[1]} \n }`;
+      return createFormWithObj;
+    }
+    return obj.value;
+  };
+
   const operator = (obj) => {
     let str = '';
     if (obj.type === 'removed') {
-      str += `\n - ${obj.name}`;
-    } else if (obj.type === 'added' || obj.type === 'updated') {
-      str += `\n + ${obj.name}`;
+      str += `\n - ${obj.name} : ${checkValue(obj)}`;
+    } else if (obj.type === 'added') {
+      str += `\n + ${obj.name} : ${checkValue(obj)}`;
     } else if (obj.type === 'unchanged') {
-      str += `\n   ${obj.name}`;
+      str += `\n   ${obj.name} : ${checkValue(obj)}`;
+    } else if (obj.type === 'updated') {
+      str += `\n + ${obj.name} : ${obj.addedValue}`;
+      str += `\n - ${obj.name} : ${obj.removedValue}`;
     }
     return str;
   };
+
   for (const obj of file) {
-    if (Array.isArray(obj.value)) {
-      const getArray = formatter(obj.value);
-      const objName = `\n ${obj.name}: ${getArray} \n}`; 
+    if (obj.type === 'parent') {
+      const getChildren = formatter(obj.value);
+      const objName = `\n ${obj.name}: ${getChildren} \n}`;
       result += objName;
-    } else if (((typeof obj.value === 'object') !== true) && ((Array.isArray(obj.value)) !== true)) {
-      const createForm = `${operator(obj)} : ${obj.value}`;
+    } else {
+      const createForm = `${operator(obj)}`;
       result += createForm;
-    } else if (typeof obj.value === 'object') {
-      const entries = (Object.entries(obj.value)).flat();
-      const createFormWithObj = `\n ${operator(obj)} : {\n ${entries[0]} : ${entries[1]} \n }`;
-      result += createFormWithObj;
     }
   }
-  const addWrapper = `{\n ${result} \n}`;
-  return addWrapper;
+  const fin = `{\n ${result} \n}`;
+  return fin;
 };
 export default formatter;
