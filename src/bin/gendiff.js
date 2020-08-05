affddcbb6975319588235cab9bfdd3166e7b5afc
Yulia Beton <yuliabeton@gmail.com>
2020-08-06 02:19:12 +0300

    feat default option format

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .version('0.0.1')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .action((before, after) => genDiff(before, after));
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((before, after) => genDiff(before, after, program.format));
 
 program.parse(process.argv);
