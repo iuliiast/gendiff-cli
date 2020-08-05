adf47a8e999014e7f7696774f4cbc3f31fc3d5ce
Yulia Beton <yuliabeton@gmail.com>
2020-08-06 01:44:11 +0300

    fix stylish

--- src/stylish.js
+++ src/stylish.js
@@ -1,9 +1,9 @@
 const formatValue = (value, depth = 1) => {
-  const makeIndent = (depth) => '  '.repeat(depth);
+  const makeIndent = () => '  '.repeat(depth);
   let acc = '';
   if (typeof value === 'object') {
     const entries = Object.entries(value).flat();
-    acc += `{\n${makeIndent(depth)}${entries[0]}: ${formatValue(entries[1])}\n${makeIndent(depth)}}`;
+    acc += `{\n${makeIndent(depth + 1)}${entries[0]}: ${formatValue(entries[1])}\n${makeIndent(depth)}}`;
   } else {
     acc += value;
   }
@@ -11,14 +11,13 @@ const formatValue = (value, depth = 1) => {
 };
 
 const formatter = (tree, depth = 1) => {
-
-  const makeIndent = (depth) => '  '.repeat(depth);
+  const makeIndent = () => '  '.repeat(depth);
 
   const formatType = (obj) => {
     const res = [];
     if (obj.type === 'parent') {
       const getChildren = formatter(obj.children, depth + 1);
-      res.push(`${makeIndent(depth)}${obj.name}: {\n${getChildren}\n${makeIndent(depth)}}`);
+      res.push(`${makeIndent(depth + 1)}${obj.name}: ${getChildren}`);
     } else if (obj.type === 'removed') {
       res.push(`${makeIndent(depth)}- ${obj.name}: ${formatValue(obj.value, depth + 1)}`);
     } else if (obj.type === 'added') {
@@ -32,6 +31,6 @@ const formatter = (tree, depth = 1) => {
   };
   const mapped = tree.map((el) => formatType(el));
   const joinMap = mapped.join('\n');
-  return joinMap;
+  return `{\n${joinMap}\n${makeIndent(depth - 1)}}`;
 };
 export default formatter;
