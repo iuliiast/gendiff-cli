b75d8320ebdebf82c9fc325eab36e6821cb45cff
Yulia Beton <yuliabeton@gmail.com>
2020-08-12 01:44:07 +0300

    fix  stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,36 +1,30 @@
 const formatValue = (value, depth = 1) => {
   const makeIndent = () => '  '.repeat(depth);
-  let acc = [];
   if (typeof value === 'object') {
     const keys = Object.keys(value);
-    keys.map((key) => {
-      depth += 1;
-      acc.push(`{\n${makeIndent(depth)}${key}: ${formatValue(value[key], depth + 1)}\n${makeIndent(depth)}}`);
-    });
-  } else {
-    acc += value;
+    const mappedKeys = keys.map((key) => `${key}: ${formatValue(value[key], depth + 1)}`);
+    const result = mappedKeys.join(`\n${makeIndent(depth)}`);
+    return `{\n${makeIndent(depth)}${result}\n${makeIndent(depth)}}`;
   }
-  return acc;
+  return `${value}`;
 };
 
 const stylish = (tree, depth = 1) => {
   const makeIndent = () => '  '.repeat(depth);
 
   const formatType = (obj) => {
-    const res = [];
     if (obj.type === 'parent') {
       const getChildren = stylish(obj.children, depth + 1);
-      res.push(`${makeIndent(depth + 1)}${obj.name}: ${getChildren}`);
-    } else if (obj.type === 'added') {
-      res.push(`${makeIndent(depth)}+ ${obj.name}: ${formatValue(obj.value, depth + 1)}`);
-    } else if (obj.type === 'removed') {
-      res.push(`${makeIndent(depth)}- ${obj.name}: ${formatValue(obj.value, depth + 1)}`);
-    } else if (obj.type === 'unchanged') {
-      res.push(`${makeIndent(depth)}  ${obj.name}: ${formatValue(obj.value, depth + 1)}`);
-    } else if (obj.type === 'updated') {
-      res.push(`${makeIndent(depth)}- ${obj.name}: ${formatValue(obj.removedValue, depth + 1)}\n${makeIndent(depth)}+ ${obj.name}: ${formatValue(obj.addedValue, depth + 1)}`);
+      return `${makeIndent(depth + 1)}${obj.name}: ${getChildren}`;
+    } if (obj.type === 'added') {
+      return `${makeIndent(depth)}+ ${obj.name}: ${formatValue(obj.value, depth + 1)}`;
+    } if (obj.type === 'removed') {
+      return`${makeIndent(depth)}- ${obj.name}: ${formatValue(obj.value, depth + 1)}`;
+    } if (obj.type === 'unchanged') {
+      return `${makeIndent(depth)}  ${obj.name}: ${formatValue(obj.value, depth + 1)}`;
+    } if (obj.type === 'updated') {
+      return `${makeIndent(depth)}- ${obj.name}: ${formatValue(obj.removedValue, depth + 1)}\n${makeIndent(depth)}+ ${obj.name}: ${formatValue(obj.addedValue, depth + 1)}`;
     }
-    return res;
   };
   const mapped = tree.map((el) => formatType(el));
   const joinMap = mapped.join('\n');
