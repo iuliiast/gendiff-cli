5245d6a89a41c5bbb724469fd3c4183c84e84dfc
Yulia Beton <yuliabeton@gmail.com>
2020-08-19 00:36:48 +0300

    refactor test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,12 +3,19 @@ import path from 'path';
 import genDiff from '../src/index';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+let readFile;
+let result;
+let resultPlain;
+
+beforeAll(() => {
+  readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+  result = readFile('resultTree.txt');
+  resultPlain = readFile('plain.txt');
+});
 
 test('gendiff json', async () => {
   const before = getFixturePath('file1.json');
   const after = getFixturePath('file2.json');
-  const result = await readFile('resultTree.txt');
   const diff = genDiff(before, after);
   expect(diff).toEqual(result);
 });
@@ -16,7 +23,6 @@ test('gendiff json', async () => {
 test('gendiff yaml', async () => {
   const before = getFixturePath('file1.yml');
   const after = getFixturePath('file2.yml');
-  const result = await readFile('result.txt');
   const diff = genDiff(before, after);
   expect(diff).toEqual(result);
 });
@@ -24,7 +30,6 @@ test('gendiff yaml', async () => {
 test('gendiff ini', async () => {
   const before = getFixturePath('file1.ini');
   const after = getFixturePath('file2.ini');
-  const result = await readFile('result.txt');
   const diff = genDiff(before, after);
   expect(diff).toEqual(result);
 });
@@ -32,7 +37,6 @@ test('gendiff ini', async () => {
 test('plain', async () => {
   const before = getFixturePath('file1.json');
   const after = getFixturePath('file2.json');
-  const result = await readFile('plain.txt');
   const diff = genDiff(before, after, 'plain');
-  expect(diff).toEqual(result);
+  expect(diff).toEqual(resultPlain);
 });
