63db500aaa0063c46241b183c81b2e1d1b76c107
yuliabeton <40248982+yuliabeton@users.noreply.github.com>
2020-08-26 13:50:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
 import fs from 'fs';
 import path from 'path';
 import runParser from './parsers';
-import formatter from './formatters/index';
+import format from './formatters/index';
 import makeDiff from './makeDiff';
 
 const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename), 'utf-8');
 
-const genDiff = (filePath1, filePath2, format = 'stylish') => {
+const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
   const data1 = readFile(filePath1);
   const data2 = readFile(filePath2);
   const obj1 = runParser(data1, path.extname(filePath1));
   const obj2 = runParser(data2, path.extname(filePath2));
   const diff = makeDiff(obj1, obj2);
-  return formatter(diff, format);
+  return format(diff, formatName);
 };
 
 export default genDiff;
